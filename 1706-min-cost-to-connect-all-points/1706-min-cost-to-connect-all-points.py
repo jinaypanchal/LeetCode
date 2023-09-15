cbe530dd29dc0661297be2ea824ed956e9363234
Jinay Panchal <36925229+jinaypanchal@users.noreply.github.com>
2023-09-15 19:30:29 -0400

    Time: 3414 ms (19.86%), Space: 152.4 MB (40.42%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -1,28 +1,40 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
+        adj = [[] for _ in range(n)]
 
-        if n ==1:
-            return 0
-        
-        def manhattan(p1, p2):
-            return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-        
-        mini_cost = 0
-        pq = [(0,0)]
-        seen = set()
-        while pq:
-            cost, u = heapq.heappop(pq)
+        def prims(adj_li, v):
+            pq = [(0,0)]
+            heapq.heapify(pq)
 
-            if u in seen:
-                continue
+            seen = set()
+            min_cost = 0
+
+            while pq:
+                cost, u = heapq.heappop(pq)
+                if u in seen:
+                    continue
+                seen.add(u)
+                min_cost += cost
+                
+                for neigh, neigh_cost in adj_li[u]:
+                    if neigh not in seen:
+                        heapq.heappush(pq, (neigh_cost, neigh))
             
-            seen.add(u)
-            mini_cost += cost
-            for v in range(n):
-                if v not in seen:
-                    heapq.heappush(pq, (manhattan(points[u], points[v]), v))
+            return min_cost
+
+        for i in range(n):
+            for j in range(i+1, n):
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+
+                d = abs(x1- x2) + abs(y1-y2)
+
+                adj[i].append((j,d))
+                adj[j].append((i,d))
         
-        return mini_cost
+        return prims(adj, n)
+
+
 
         
