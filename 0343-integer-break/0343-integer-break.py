2ce2b87965a33fff11c2bed11e04bf792359911c
Jinay Panchal <36925229+jinaypanchal@users.noreply.github.com>
2023-10-06 13:27:48 -0400

    Time: 48 ms (12.51%), Space: 16.2 MB (58.98%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,16 +1,21 @@
 class Solution:
-    @cache
+    def __init__(self):
+        self.memo = {}
+    # @cache
     def f(self, n):
-        # memo = {}
         if n == 1:
             return 1
+        
+        if n in self.memo:
+            return self.memo[n]
         
         res = float('-inf')
         for i in range(1, n):
             prod = i * max(n-i, self.f(n-i))
             res = max(res, prod)
         
-        return res
+        self.memo[n] = res
+        return self.memo[n]
         
     def integerBreak(self, n: int) -> int: 
         return self.f(n)
