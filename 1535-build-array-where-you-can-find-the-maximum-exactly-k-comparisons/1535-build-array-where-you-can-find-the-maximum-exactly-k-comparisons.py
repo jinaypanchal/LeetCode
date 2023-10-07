8c980c43393def7db15888eb1f1ab79ebe9cad5e
Jinay Panchal <36925229+jinaypanchal@users.noreply.github.com>
2023-10-07 19:34:39 -0400

    Time: 3242 ms (6.66%), Space: 43.1 MB (10.48%) - LeetHub

---
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numOfArrays(self, n: int, m: int, k: int) -> int:
+        mod = 10**9 + 7
+        memo = {}
+        def f(idx, sc, maxi):
+            res = 0
+            if idx == n:
+                if sc == k:
+                    return 1
+                else:
+                    return 0
+
+            if (idx, sc, maxi) in memo:
+                return memo[(idx, sc, maxi)]
+            
+            for i in range(1, m+1):
+                if i > maxi:
+                    res += f(idx+1, sc+1, i)
+                else:
+                    res += f(idx+1, sc, maxi)
+
+            memo[(idx, sc, maxi)] = res%mod         
+            return memo[(idx, sc, maxi)]
+        
+        return f(0, 0, -1)
+
+
+        
