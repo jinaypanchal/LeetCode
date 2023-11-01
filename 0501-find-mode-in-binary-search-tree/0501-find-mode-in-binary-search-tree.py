0daf56f8289d1d31d9b6091d6d01811d486fe2a4
Jinay Panchal <36925229+jinaypanchal@users.noreply.github.com>
2023-11-01 00:34:44 -0400

    Time: 49 ms (89.59%), Space: 20.2 MB (94.89%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -6,35 +6,68 @@
 #         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        # if root == [0]:
-        #     return [0]
-
-        d = {}
         def inorder(node):
+            nonlocal maxi, prev, ct, mode
             if not node:
-                return []
-            
-            inorder(node.left)
-            if node.val not in d:
-                d[node.val] = 1
+                return
+                        
+            inorder(node.left)            
+            if node.val == prev:
+                ct += 1
             else:
-                d[node.val] += 1
-            inorder(node.right)
-        inorder(root)
-        # print(d)
-        sorted_dict = dict(sorted(d.items(), key = lambda x:x[1]))
-        print(sorted_dict)
-        res = []
-        maxi = max(sorted_dict.values())
-        for key,val in sorted_dict.items():
-            if val == maxi:
-                res.append(key)
+                ct = 1
+            
+            if ct > maxi:
+                maxi = ct
+                mode = [node.val]
+            elif ct == maxi:
+                mode.append(node.val)
+            
+            prev = node.val
+            inorder(node.right)      
         
-        return res
+        
+        maxi = 0
+        prev = None
+        ct = 0
+        mode = []
+        inorder(root)
+        return mode
+            
 
 
-        #  res = []
-        #  for key, val in sorted_dict.items():
-        #      if 
+
+        # # if root == [0]:
+        # #     return [0]
+
+        # d = {}
+        # def inorder(node):
+        #     if not node:
+        #         return []
+            
+        #     inorder(node.left)
+        #     if node.val not in d:
+        #         d[node.val] = 1
+        #     else:
+        #         d[node.val] += 1
+        #     inorder(node.right)
+        # inorder(root)
+        # # print(d)
+        # sorted_dict = dict(sorted(d.items(), key = lambda x:x[1]))
+        # print(sorted_dict)
+        # res = []
+        # maxi = max(sorted_dict.values())
+
+        
+        # for key,val in sorted_dict.items():
+        #     if val == maxi:
+        #         res.append(key)
+        
+        # return res
+
+
+        # #  res = []
+        # #  for key, val in sorted_dict.items():
+        # #      if 
         
 
