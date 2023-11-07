216fdd27594c2c20bb99223b7ba26f39a3124a5f
Jinay Panchal <36925229+jinaypanchal@users.noreply.github.com>
2023-11-07 00:39:46 -0500

    Time: 1557 ms (54.65%), Space: 71.3 MB (32.92%) - LeetHub

--- 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.py
@@ -1,34 +1,39 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+
         losser = {}
         winner = {}
         for i in range(len(matches)):
-            if matches[i][0] not in winner:
-                winner[matches[i][0]] = 1
-            else:
-                winner[matches[i][0]] += 1
+            # if matches[i][0] not in winner:
+            #     winner[matches[i][0]] = 1
+            # else:
+            #     winner[matches[i][0]] += 1
 
             if matches[i][1] not in losser:
                 losser [matches[i][1]] = 1
             else:
                 losser[matches[i][1]] += 1
-        
-        print(winner)
-        print(losser)
         w = []
-        for key, val in winner.items():
-            if key not in losser:
-                w.append(key)
+        for t in matches:
+            if t[0] not in losser:
+                w.append(t[0])       
+        
+        # print(winner)
+        # print(losser)
+        # w = []
+        # for key, val in winner.items():
+        #     if key not in losser:
+        #         w.append(key)
         
         
         l = []
         for key, val in losser.items():
             if val == 1:
                 l.append(key)
-                
-        w = sorted(w)
+
+        # w = sorted(w)
         l = sorted(l)
-        return [w, l] 
+        return [list(sorted(set(w))), l] 
 
         
 
