0bee454ffe9f1710c5e5582206f499fa34343f70
Jinay Panchal <36925229+jinaypanchal@users.noreply.github.com>
2023-12-15 01:33:45 -0500

    Time: 58 ms (78.03%) | Memory: 16.4 MB (45.28%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -1,13 +1,24 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        s = set()
         city = ""
+        d = {}
         for i in paths:
-            s.add(i[0])
+            d[i[0]] = d.get(0, i[1])
         
         for i in paths:
-            if i[1] not in s:
+            if i[1] not in d:
                 city = i[1]
                 break
-        return i[1]
+        return city
+
+        # s = set()
+        # city = ""
+        # for i in paths:
+        #     s.add(i[0])
+        
+        # for i in paths:
+        #     if i[1] not in s:
+        #         city = i[1]
+        #         break
+        # return city
         
